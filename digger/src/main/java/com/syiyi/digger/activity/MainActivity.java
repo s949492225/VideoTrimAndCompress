e77a1e4d143baafcb348704eeef9d2678a9fb6d1
wujun <jun.wu@lrlz.com>
2017-07-13 15:11:24 +0800

    ``

---
+++ digger/src/main/java/com/syiyi/digger/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.syiyi.digger.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.syiyi.digger.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
